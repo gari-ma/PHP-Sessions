5bb772e28db39b81e266906fcb52563021938d89
npgarima <thegarimaweb@gmail.com>
2023-04-04 00:07:16 +0545

    added css

--- authorization/home.php
+++ authorization/home.php
@@ -5,37 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Authorization</title>
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" type="text/css" href="authorization/home.css">
 </head>
 <body>
+
 <?php
 
-$var="admin";
+$var="subscriber";
 if($var=="admin"){    
 ?>
 
-<p>
-    Hello
-</p>
-
 <div class="user"> Hello Admin </div>
-
-<?php
-}
-elseif($var=="subscriber"){
-    ?>
-    <div class="user"> Hello Subscriber</div>
-    <?php
-}
-else{
-    ?>
-    <div class="user"> Hello Unauthorised person</div>
-    <?php
-}
-
-if($var=="admin"||$var=="subscriber"){
-    ?>
-    <div class="match"> Authorization Matched</div>
+<div class="match"> Authorization Matched</div>
     <div class="box"> 
         <h6>1. Access granted as admin</h6>
         <h5>2. Access granted as admin</h5>
@@ -44,23 +25,35 @@ if($var=="admin"||$var=="subscriber"){
         <h2>5. Access granted as admin</h2>
     </div>
 
-
+<?php
+}
+elseif($var=="subscriber"){
+    ?>
+    <div class="user"> Hello Subscriber</div>
+    <div class="match"> Authorization Matched</div>
+    <div class="box"> 
+        <h6>1. Access granted as subscriber</h6>
+        <h5>2. Access granted as subscriber</h5>
+        <h4>3. Access granted as subscriber</h4>
+        <h3>4. Access granted as subscriber</h3>
+        <h2>5. Access granted as subscriber</h2>
+    </div>
+    
     <?php
 }
 else{
     ?>
+    <div class="user"> Hello Unauthorised person</div>
     <div class="match"> Authorization Not Matched</div>
     <div class="box"> 
-        <h6>1. Access not granted as admin</h6>
-        <h5>2. Access not granted as admin</h5>
-        <h4>3. Access not granted as admin</h4>
-        <h3>4. Access not granted as admin</h3>
-        <h2>5. Access not granted as admin</h2>
+        <h6>1. ENTER YOUR CRENDTIALS AGAIN </h6>
+        <h5>2. ENTER YOUR CRENDTIALS AGAIN </h5>
+        <h4>3. ENTER YOUR CRENDTIALS AGAIN </h4>
+        <h3>4. ENTER YOUR CRENDTIALS AGAIN </h3>
+        <h2>5. ENTER YOUR CRENDTIALS AGAIN </h2>
     </div>
     <?php
-}
-
-?>
+}?>
 
 </body>
 </html>
