4c9402369202da623cafcfb6d8f79949b12d9f5a
npgarima <thegarimaweb@gmail.com>
2023-04-04 12:37:08 +0545

    updated minimized code

--- authorization/home.php
+++ authorization/home.php
@@ -5,55 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Authorization</title>
-    <link rel="stylesheet" type="text/css" href="authorization/home.css">
+    <link rel="stylesheet" type="text/css" href="home.css">
 </head>
 <body>
 
 <?php
+    // variables declarations
+    $var="admin";
+    $authMessage = "";
+    $nextMessage = "";
 
-$var="subscriber";
-if($var=="admin"){    
+    // case check
+    if($var=="admin"){    
+        $authMessage = "Authorization matched";
+        $nextMessage = "Access granted as admin";
+    }
+    elseif($var=="subscriber"){
+        $authMessage = "Authorization matched";
+        $nextMessage = "You are subscriber";
+    }
+    else {
+        $authMessage = "Authorization not matched";
+        $nextMessage = "Enter your credentials";
+    }
 ?>
+<div classs="main"> 
+    <div class="user"> Hello <?php echo $var;?> </div>
+    <div class="match"> <?php echo $authMessage;?></div>
+    <div class="box"> 
+        <h5>1. <?php echo $nextMessage; ?></h5><br>
+        <h4>2. <?php echo $nextMessage; ?></h4><br>
+        <h3>3. <?php echo $nextMessage; ?></h3><br>
+        <h2>4. <?php echo $nextMessage; ?></h2><br>
+        <h1>5. <?php echo $nextMessage; ?></h1><br>
+    </div>
+</div>
 
-<div class="user"> Hello Admin </div>
-<div class="match"> Authorization Matched</div>
-    <div class="box"> 
-        <h6>1. Access granted as admin</h6>
-        <h5>2. Access granted as admin</h5>
-        <h4>3. Access granted as admin</h4>
-        <h3>4. Access granted as admin</h3>
-        <h2>5. Access granted as admin</h2>
-    </div>
-
-<?php
-}
-elseif($var=="subscriber"){
-    ?>
-    <div class="user"> Hello Subscriber</div>
-    <div class="match"> Authorization Matched</div>
-    <div class="box"> 
-        <h6>1. Access granted as subscriber</h6>
-        <h5>2. Access granted as subscriber</h5>
-        <h4>3. Access granted as subscriber</h4>
-        <h3>4. Access granted as subscriber</h3>
-        <h2>5. Access granted as subscriber</h2>
-    </div>
-    
-    <?php
-}
-else{
-    ?>
-    <div class="user"> Hello Unauthorised person</div>
-    <div class="match"> Authorization Not Matched</div>
-    <div class="box"> 
-        <h6>1. ENTER YOUR CRENDTIALS AGAIN </h6>
-        <h5>2. ENTER YOUR CRENDTIALS AGAIN </h5>
-        <h4>3. ENTER YOUR CRENDTIALS AGAIN </h4>
-        <h3>4. ENTER YOUR CRENDTIALS AGAIN </h3>
-        <h2>5. ENTER YOUR CRENDTIALS AGAIN </h2>
-    </div>
-    <?php
-}?>
 
 </body>
 </html>
